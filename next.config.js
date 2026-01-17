3d3ca938ffb3de431e956a307a12a3c2d8f064b3
Kaan Koç <kaankoc@192.168.1.7>
2026-01-17 18:32:48 +0300

    Add Wikimedia to allowed image domains for Google logos

--- next.config.js
+++ next.config.js
@@ -17,6 +17,11 @@ const nextConfig = {
         hostname: 'cdn.pixabay.com',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        pathname: '/**',
+      },
     ],
   },
   experimental: {
